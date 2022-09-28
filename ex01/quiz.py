79d5d8fc633bad38fb8783bbcb0ed591ab029cbd
sota <c0b210090c@edu.teu.ac.jp>
2022-09-28 10:39:31 +0900

    quiz final

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,17 +1,27 @@
 import random
-import datetime
+import time
+def syutudai(quiz):
+    qa = random.choice(quiz)
+    print("問題：\n"+qa["q"])
+    return qa["a"]
+
+def kaito(ansList):
+    ans = input("答えるんだ：")
+    if ans in ansList:
+        print("正解")
+    else:
+        print("不正解")
+        
 if __name__ == "__main__":
     a = random.randint(0,2)
-    quizList = ["サザエさんの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
-    ansList = [["ますお","マスオ"],["わかめ","ワカメ"],["おい","甥","甥っ子","おいっこ"]]
-    print("問題：\n"+quizList[a])
-    st = datetime.datetime.now
-    ans = input('答えるんだ：')
-    ed = datetime.datetime.now
-    if ans in ansList[a]:
-        print('正解！！！')
-    else:
-        print('出直してこい')
+    quizList = [{"q":"サザエさんの旦那の名前は？", "a":["ますお","マスオ"]},
+                {"q":"カツオの妹の名前は？", "a":["わかめ","ワカメ"]},
+                {"q":"タラオはカツオから見てどんな関係？", "a":["おい","甥","甥っ子","おいっこ"]}]
+    st = time.time()
+    ans_lst = syutudai(quizList)
+    kaito(ans_lst)
+    ed = time.time()
+    print(ed-st)
    
     
     
