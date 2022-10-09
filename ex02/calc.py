e5c41646f64f8ef154cd27d17fe363a7c1c42b69
sota <c0b210090c@edu.teu.ac.jp>
2022-10-09 22:10:47 +0900

    4,5

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,13 +3,18 @@ import tkinter.messagebox as tkm
 
 def btn_click(event):
     btn=event.widget
-    txt=btn["text"]
+    txt=int(btn["text"])
     tkm.showinfo("", f"[{txt}]のボタンがクリックされました")
+    entry.insert(tk.END, txt)
     return "break"
 
 root = tk.Tk()
 root.title("tk")
 root.geometry("300x500")
+
+entry = tk.Entry(justify="right",width=10,font=("Times New Roman", 40))
+#justify 右揃え
+entry.grid(columnspan=30)
 
 for i in range(10):
     btn = tk.Button(root, text=f"{i}", font=("Times New Roman", 30), width=4, height=2)
