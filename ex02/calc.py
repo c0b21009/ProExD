0dffe9c5be8e9c96e43166f37fdecf8ea946a7e8
sota <c0b210090c@edu.teu.ac.jp>
2022-10-09 22:29:07 +0900

    6

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,16 +3,17 @@ import tkinter.messagebox as tkm
 
 def btn_click(event):
     btn=event.widget
-    txt=int(btn["text"])
-    tkm.showinfo("", f"[{txt}]のボタンがクリックされました")
+    txt=btn["text"]
+    tkm.showinfo(f"{txt}", f"[{txt}]のボタンがクリックされました")
     entry.insert(tk.END, txt)
     return "break"
+    #0tk.ENDを0にすると2つ目の入力が十の位になる
 
 root = tk.Tk()
 root.title("tk")
-root.geometry("300x500")
+root.geometry("300x600")
 
-entry = tk.Entry(justify="right",width=10,font=("Times New Roman", 40))
+entry = tk.Entry(root, justify="right",width=10,font=("Times New Roman", 40))
 #justify 右揃え
 entry.grid(columnspan=30)
 
@@ -30,5 +31,7 @@ for i in range(10):
         r=1
     c=3-(i-1)%3
     btn.grid(row=r, column=c)
-
+btn = tk.Button(root, text="+", font=("Times New Roman", 30), width=4, height=2)
+btn.bind("<1>", btn_click)
+btn.grid(row=4, column=2)
 root.mainloop()
