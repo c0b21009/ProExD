5b390df5507b9e8745d02e749e46c97c4e099863
sota <c0b210090c@edu.teu.ac.jp>
2022-10-18 15:06:00 +0900

    8

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,10 +1,28 @@
 import tkinter as tk
-    
+import maze_maker as mm #8
 def key_down(event):
     global key
     key = event.keysym#5
     print(key)
+    
+def key_up(event):
+    global key
+    key = ""#6
 
+def main_proc():
+    
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+    if key =="Down":
+        cy += 20
+    if key == "Left":
+        cx -= 20
+    if key =="Right":
+        cx += 20
+    can.coords("tori", cx, cy)
+    root.after(100, main_proc)#7
+    
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")#1
@@ -18,4 +36,5 @@ if __name__ == "__main__":
     
     key = "" #4
     root.bind("<KeyPress>", key_down)#5
+    root.bind("<KeyRelease", key_up)#6
     root.mainloop()
