2b6d2267eb4ee25f6f357e8f29e0901c9497c388
sota <c0b210090c@edu.teu.ac.jp>
2022-10-18 17:10:12 +0900

    add01

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 import maze_maker as mm #8
+import tkinter.messagebox as tkm
+import time
+import random
 
 # 5
 def key_down(event):
@@ -17,6 +20,12 @@ def key_up(event):
 def main_proc():
     global mx, my
     global cx, cy
+    global lx, ly
+    global tmr,tmr2
+    global px, py
+    tmr +=1
+    tmr2 +=0.1
+    
     if key == "Up":
         my -= 1
     if key == "Down":
@@ -36,11 +45,33 @@ def main_proc():
             mx += 1
         if key == "Right":
             mx -= 1
+    if tmr>130:
+        tmr=0
+    lx=(tmr%15)*100+50
+    ly=(tmr//15)*100+50
+    px=random.randint(0, 1500)
+    py=random.randint(0, 900)
+    
     canv.coords("tori", cx, cy)
+    canv.coords("lion", lx, ly)
+    canv.coords("pen", px, py)
+    
+    if lx==cx and ly==cy:
+        tkm.showwarning("危ない！", "食べられちゃったー"+str(tmr2)+"秒逃げたよ")
+        tori = tk.PhotoImage(file="fig/5.png") 
+        mx, my = 1, 1
+        cx, cy = mx*100+50, my*100+50
+        canv.create_image(cx, cy, image=tori, tag="tori")
+        tmr=0
+        tmr2=0.0
+    
+        
     root.after(100, main_proc)
 
 
 if __name__ == "__main__":
+    tmr=0
+    tmr2=0.0
     root = tk.Tk()
     root.title("迷えるこうかとん") # 1
 
@@ -58,6 +89,14 @@ if __name__ == "__main__":
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50
     canv.create_image(cx, cy, image=tori, tag="tori")
+    
+    lion = tk.PhotoImage(file="fig/3926.png") 
+    lx, ly = 100-50, 100-50
+    canv.create_image(lx, ly, image=lion, tag="lion")
+    
+    pen = tk.PhotoImage(file="fig/3926.png") 
+    px, py =0,0
+    canv.create_image(px, py, image=pen, tag="pen")
 
     # 4
     key = "" 
