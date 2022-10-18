d367c78f79938e70fcb381750154390a4b3a0002
sota <c0b210090c@edu.teu.ac.jp>
2022-10-18 14:45:36 +0900

    4

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,16 +1,17 @@
 import tkinter as tk
+    
+    
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん")
+    root.title("迷えるこうかとん")#1
     
     can = tk.Canvas(root, width=1500, height=900,bg="black")
-    can.pack()
+    can.pack()#2
     
     tori = tk.PhotoImage(file="fig/1.png")
     cx, cy =300, 400
-    can.create_image(cx, cy, image=tori, tag="tori")
+    can.create_image(cx, cy, image=tori, tag="tori")#3
+    
+    key = ""#4
     root.mainloop()
-    
-    
-    
